0c52f6363d8f21020e108bebc16cd091f4d67081
jose221 <ext.angel.alvarado@pricetravel.com>
2025-08-08 01:52:25 -0500

    refactor: remove legacy CV editor components and templates

--- resources/js/components/vitae/editor/contexts/PreviewModeContext.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useContext } from 'react';
-
-// Create Context for Preview Mode
-export const PreviewModeContext = createContext(false);
-
-// Hook to use preview mode context
-export const usePreviewMode = () => {
-    return useContext(PreviewModeContext);
-};
